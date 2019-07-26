1203c1ad53af6aff289827645c68f7850f19cdf8
takaaki sato <takaaki.sato@zozo.com>
2019-07-26 23:21:34 +0900

    modify main.mjs

--- main.mjs
+++ main.mjs
@@ -1,23 +1,38 @@
 import puppeteer from 'puppeteer';
 import moment from 'moment';
 import dotenv from 'dotenv';
+
 dotenv.config();
 const CONFING = process.env;
 
 (async () => {
-	const browser = await puppeteer.launch({
-		headless: false,
-		slowMo: 50
-	});
-	const page = await browser.newPage();
-	await page.setViewport({
-		width: 1200,
-		height: 800
-	});
-	await page.goto(`${CONFING.TARGET_URL}`);
-	
-	await page.screenshot({ path: `./screenshot/${moment().format('YYYYMMDDHHmmssSSS')}.png`, fullPage: true });
-	await browser.close();
+  const browser = await puppeteer.launch({
+    headless: true,
+    slowMo: 50,
+  });
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 1200,
+    height: 800,
+  });
+  await page.goto(`${CONFING.TARGET_URL}`);
+  await page.click('a[aria-label*="在庫あり"]');
+
+  await page._client.send(
+    'Input.synthesizeScrollGesture',
+    {
+      x: 0,
+      y: 0,
+      xDistance: 0,
+      yDistance: -700,
+      repeatCount: 5,
+      repeatDelayMs: 200,
+    },
+  );
+
+  await page.screenshot({ path: `./screenshot/${moment().format('YYYYMMDDHHmmssSSS')}.png`, fullPage: true });
+  await browser.close();
 })();
 
 process.on('unhandledRejection', console.dir);
